81d8f29155fbab27dbe8c31cb8dc0cd16c946a91
BlinkMo <mengmo2008@163.com>
2016-05-18 11:31:58 +0800

    add pad
    add pad

--- BugzillaTool/background.js
+++
@@ -1,3 +0,0 @@
-chrome.pageAction.onClicked.addListener(function (tab) {
-      chrome.tabs.executeScript(null,{file:"popup.js"});
-});
--- BugzillaTool/popup.js
+++ BugzillaTool/popup.js
@@ -6,18 +6,35 @@ document.addEventListener("DOMContentLoaded", function () {
   var iVersionReport_url = "https://bugzilla.corp.qihoo.net/report.cgi?x_axis_field=bug_severity&y_axis_field=bug_status&z_axis_field=&query_format=report-table&short_desc_type=allwordssubstr&short_desc=&classification=360%E6%99%BA%E8%83%BD%E6%9C%BA%E5%99%A8%E4%BA%BA&product=360%E6%99%BA%E8%83%BD%E6%91%84%E5%83%8F%E6%9C%BA&component=IOS%E5%AE%A2%E6%88%B7%E7%AB%AF&version="+localStorage.getItem("iosText")+"&longdesc_type=allwordssubstr&longdesc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&emailtype1=substring&email1=&emailtype2=substring&email2=&bug_id_type=anyexact&bug_id=&chfieldfrom=&chfieldto=Now&chfieldvalue=&format=table&action=wrap&field0-0-0=noop&type0-0-0=noop&value0-0-0=";
   var rVersionReport_url = "https://bugzilla.corp.qihoo.net/report.cgi?x_axis_field=bug_severity&y_axis_field=bug_status&z_axis_field=&query_format=report-table&short_desc_type=allwordssubstr&short_desc=&classification=360%E6%99%BA%E8%83%BD%E6%9C%BA%E5%99%A8%E4%BA%BA&product=360%E6%99%BA%E8%83%BD%E6%91%84%E5%83%8F%E6%9C%BA&component=%E5%9B%BA%E4%BB%B6ROM&longdesc_type=allwordssubstr&longdesc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&emailtype1=substring&email1=&emailtype2=substring&email2=&bug_id_type=anyexact&bug_id=&chfieldfrom=&chfieldto=Now&chfieldvalue=&format=table&action=wrap&field0-0-0=noop&type0-0-0=noop&value0-0-0=";
 
-  buttonClick("aVersionAll","a","ALL","0");
-  buttonClick("iVersionAll","i","ALL","0");
-  buttonClick("rVersionAll","r","ALL","0");
+  buttonClick("aVersionAll","a","ALL","0","CAMERA");
+  buttonClick("iVersionAll","i","ALL","0","CAMERA");
+  buttonClick("rVersionAll","r","ALL","0","CAMERA");
 
-  buttonClick("aVersionNew","a","NEW","0");
-  buttonClick("iVersionNew","i","NEW","0");
+  buttonClick("aVersionNew","a","NEW","0","CAMERA");
+  buttonClick("iVersionNew","i","NEW","0","CAMERA");
 
-  buttonClick("aVersionRESO","a","RESOLVED","0");
-  buttonClick("iVersionRESO","i","RESOLVED","0");
+  buttonClick("aVersionRESO","a","RESOLVED","0","CAMERA");
+  buttonClick("iVersionRESO","i","RESOLVED","0","CAMERA");
 
-  buttonClick("aDailyCreate","a","ALL","1");
-  buttonClick("iDailyCreate","i","ALL","1");
+  buttonClick("aDailyCreate","a","ALL","1","CAMERA");
+  buttonClick("iDailyCreate","i","ALL","1","CAMERA");
+
+  buttonClick("robot_aVersionAll","ra","ALL","0","ROBOT");
+  buttonClick("robot_iVersionAll","ri","ALL","0","ROBOT");
+  buttonClick("robot_rVersionAll","rr","ALL","0","ROBOT");
+
+  buttonClick("robot_aVersionNew","ra","NEW","0","ROBOT");
+  buttonClick("robot_iVersionNew","ri","NEW","0","ROBOT");
+  buttonClick("robot_rVersionNew","rr","NEW","0","ROBOT");
+
+  buttonClick("robot_aVersionRESO","ra","RESOLVED","0","ROBOT");
+  buttonClick("robot_iVersionRESO","ri","RESOLVED","0","ROBOT");
+  buttonClick("robot_rVersionRESO","rr","RESOLVED","0","ROBOT");
+
+  buttonClick("robot_aDailyCreate","ra","ALL","1","ROBOT");
+  buttonClick("robot_iDailyCreate","ri","ALL","1","ROBOT");
+  buttonClick("robot_rDailyCreate","rr","ALL","1","ROBOT");
+
 
   document.getElementById("aVersionReport").addEventListener("click", function () {
     chrome.tabs.create({ url: aVersionReport_url });
@@ -29,14 +46,12 @@ document.addEventListener("DOMContentLoaded", function () {
     chrome.tabs.create({ url: rVersionReport_url });
   });
 
-  document.getElementById("androidChange").addEventListener("click", function () {
-    localStorage.setItem("androidText",document.getElementById("androidText").value);
-    location.reload();
-  });
-  document.getElementById("iosChange").addEventListener("click", function () {
-    localStorage.setItem("iosText",document.getElementById("iosText").value);
-    location.reload();
-  });
+  ChangeButtonClick("androidChange","androidText");
+  ChangeButtonClick("iosChange","iosText");
+  ChangeButtonClick("robot_androidChange","robot_androidText");
+  ChangeButtonClick("robot_iosChange","robot_iosText");
+  ChangeButtonClick("robot_romChange","robot_romText");
+
 });
 
 
@@ -54,13 +69,17 @@ function loadLocalStorage(){
   {
     document.getElementById("androidText").value = localStorage.getItem("androidText");
     document.getElementById("iosText").value = localStorage.getItem("iosText");
+    document.getElementById("robot_androidText").value = localStorage.getItem("robot_androidText");
+    document.getElementById("robot_iosText").value = localStorage.getItem("robot_iosText");
+    document.getElementById("robot_romText").value = localStorage.getItem("robot_romText");
   }
   else {
     localStorage.setItem("BugzillaTool","true");
-    localStorage.setItem("androidText","A5.3.2.80");
-    localStorage.setItem("iosText","I5.3.3");
-    document.getElementById("androidText").value = "A5.3.2.80";
-    document.getElementById("iosText").value = "I5.3.3";
+    localStorageSet("androidText","A5.3.2.80");
+    localStorageSet("iosText","I5.3.3");
+    localStorageSet("robot_androidText","1.0.0.1001");
+    localStorageSet("robot_iosText","1.0.0.1001");
+    localStorageSet("robot_romText","1.0.0.1001");
   }
 }
 
@@ -79,10 +98,11 @@ function CurrentTime(){
   return ctime;
 }
 
-function buttonClick(id,type,status,fieldform){
+function buttonClick(id,type,status,fieldform,prod){
   var version = "&version=";
   var component = "&component=";
   var bug_status = "&bug_status="+status;
+  var product = "&product=";
 
   var cTime = CurrentTime();
   var chfieldfrom = "";
@@ -94,9 +114,21 @@ function buttonClick(id,type,status,fieldform){
     version += localStorage.getItem("iosText");
     component += "IOS%E5%AE%A2%E6%88%B7%E7%AB%AF";
   }
-  else{
+  else if (type == 'r'){
     version = "";
     component += "%E5%9B%BA%E4%BB%B6ROM";
+  }
+  else if(type == 'ra'){
+    version += localStorage.getItem("robot_androidText");
+    component += "手机android端";
+  }
+  else if (type == 'ri') {
+    version += localStorage.getItem("robot_iosText");
+    component += "手机IOS端";
+  }
+  else {
+    version += localStorage.getItem("robot_romText");
+    component += "平板APK%26ROM";
   }
   if(status == "ALL"){
     bug_status = "&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED";
@@ -104,9 +136,27 @@ function buttonClick(id,type,status,fieldform){
   if(fieldform == "1"){
     chfieldfrom = "&chfield=%5BBug%20creation%5D&chfieldfrom="+cTime;
   }
+  if(prod == "ROBOT"){
+    product += "%E5%B1%8F%E5%B9%95%E6%9D%BF";
+  }
+  else {
+    product += "360%E6%99%BA%E8%83%BD%E6%91%84%E5%83%8F%E6%9C%BA";
+  }
 
   document.getElementById(id).addEventListener("click", function () {
-    var buttonClickUrl = "https://bugzilla.corp.qihoo.net/buglist.cgi?query_format=advanced"+chfieldfrom+bug_status+version+component+"&product=360%E6%99%BA%E8%83%BD%E6%91%84%E5%83%8F%E6%9C%BA&classification=360%E6%99%BA%E8%83%BD%E6%9C%BA%E5%99%A8%E4%BA%BA";
+    var buttonClickUrl = "https://bugzilla.corp.qihoo.net/buglist.cgi?query_format=advanced"+chfieldfrom+bug_status+version+component+product+"&classification=360%E6%99%BA%E8%83%BD%E6%9C%BA%E5%99%A8%E4%BA%BA";
     chrome.tabs.create({ url: buttonClickUrl });
   });
+}
+
+function ChangeButtonClick(changeId,textId){
+  document.getElementById(changeId).addEventListener("click", function () {
+    localStorage.setItem(textId,document.getElementById(textId).value);
+    location.reload();
+  });
+}
+
+function localStorageSet(key,value){
+  localStorage.setItem(key,value);
+  document.getElementById(key).value = value;
 }
